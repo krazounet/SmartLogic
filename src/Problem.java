0b577d7d2a306f3e3f65be373aea9f240cae1f04
pacific23 <48202212+pacific23@users.noreply.github.com>
2021-06-10 12:09:47 +0200

    Export de la solution

--- src/Problem.java
+++ src/Problem.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import Enum.*;
@@ -95,11 +96,64 @@ public class Problem {
 
         return retour;
     }
+    
+    public void exportSolution(String filename)
+    {
+        BufferedImage tableau = this.createTableau(false, true);
+        
+        for(int m = 0; m < ConfigPartie.list_moments_partie.size(); m++)
+        {
+        	Moment ceMoment = ConfigPartie.list_moments_partie.get(m);
+        	for(int p = 0; p < ConfigPartie.list_pieces_partie.size(); p++)
+        	{
+        		Piece cettePiece = ConfigPartie.list_pieces_partie.get(p);
+        		List<Personne> listPersonnesHere = new ArrayList<Personne>();
+        		
+        		for(Reservation reservationToTest : solution.solution)
+        		{
+        			if(reservationToTest.moment == ceMoment && reservationToTest.lieu.piece == cettePiece)
+        			{
+        				listPersonnesHere.add(reservationToTest.personne);
+        			}
+        		}
+        		
+        		int centerCaseX = 250 + m * 100;
+        		int centerCasey = 150 + p * 100;
+        		
+        		BufferedImage caseToDraw = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
+        		
+				List<List<Point>> listListPosCharacter = Arrays.asList(
+						Arrays.asList(new Point(2, 2)),
+						Arrays.asList(new Point(1, 2), new Point(3, 2)),
+						Arrays.asList(new Point(1, 1), new Point(3, 1), new Point(2, 3)),
+						Arrays.asList(new Point(1, 1), new Point(3, 1), new Point(1, 3), new Point(3, 3)),
+						Arrays.asList(new Point(1, 1), new Point(3, 1), new Point(2, 2), new Point(1, 3), new Point(3, 3)),
+						Arrays.asList(new Point(1, 1), new Point(3, 1), new Point(1, 2), new Point(3, 2), new Point(1, 3), new Point(3, 3))
+						);
+				
+				for(int c = 0; c < listPersonnesHere.size(); c++)
+					DrawTools.drawImageTransformed(caseToDraw.getGraphics(), DrawTools.getImage("Image/" + listPersonnesHere.get(c).name() + ".png"), 25 * listListPosCharacter.get(listPersonnesHere.size() - 1).get(c).x, 25 * listListPosCharacter.get(listPersonnesHere.size() - 1).get(c).y, 0, 50);
+				
+				DrawTools.drawImageCenter(tableau.getGraphics(), caseToDraw, centerCaseX, centerCasey);
+
+        	}
+        }
+        
+        DrawTools.saveFile(tableau, SmartLogic.repertoire+"export/" + filename + ".png");
+    }
+    
+    public void exportVierge(String filename)
+    {
+        BufferedImage tableau = this.createTableau(false, false);
+        
+        DrawTools.saveFile(tableau, SmartLogic.repertoire+"export/" + filename + ".png");
+    }
+    
     public void export(String filename, boolean onlyUsefull){
 
         BufferedImage fond = DrawTools.getImage(SmartLogic.repertoire+"Image\\FondSmartLogic.png");
         DrawTools.drawImageTransformed(fond.getGraphics(),plan.export(),500,500,0,100);
-        BufferedImage tableau = this.createTableau();
+        BufferedImage tableau = this.createTableau(true, true);
         int ybase=1050;
         int y_hors_tableau=1300;//positionnnement de depart pour les infos qui ne rentre pas dans le tableau
         for(int i=0;i<list_indices.size();i++){
@@ -149,7 +203,7 @@ public class Problem {
         return true;
     }
 
-    public BufferedImage createTableau (){
+    public BufferedImage createTableau (boolean displayBorder, boolean displayRoads){
         //image carre
         BufferedImage img_carre = DrawTools.getImage(SmartLogic.repertoire+"Image\\carre.png");
         BufferedImage img_carre_bord = DrawTools.getImage(SmartLogic.repertoire+"Image\\carre_bord.png");
@@ -157,22 +211,25 @@ public class Problem {
         BufferedImage image_tableau = new BufferedImage((ConfigPartie.nombre_moment+2)*100 + 100,(ConfigPartie.nombre_lieu+2)*100,BufferedImage.TYPE_INT_ARGB);
         Graphics graph_tableau= image_tableau.getGraphics();
         
-        Graphics2D g2 = (Graphics2D) graph_tableau;
-        g2.setColor(Color.BLACK);
-		g2.setStroke(new BasicStroke(7));
-        for(Conexion conexionToDraw : plan.list_conexions)
+        if(displayRoads)
         {
-        	int startY1 = ConfigPartie.list_pieces_partie.indexOf(conexionToDraw.Piece1);
-        	int startY2 = ConfigPartie.list_pieces_partie.indexOf(conexionToDraw.Piece2);
-        	if(startY1 > startY2)
-        	{
-        		int temp = startY1;
-        		startY1 = startY2;
-        		startY2 = temp;
-        	}
-        	int gap = startY2 - startY1;
-
-        	g2.drawArc((120 - (gap * 10)) - (10 * gap),  150 + startY1 * 100, (120 - (gap * 10)) + (10 * gap), gap * 100, 90, 180);
+	        Graphics2D g2 = (Graphics2D) graph_tableau;
+	        g2.setColor(Color.BLACK);
+			g2.setStroke(new BasicStroke(7));
+	        for(Conexion conexionToDraw : plan.list_conexions)
+	        {
+	        	int startY1 = ConfigPartie.list_pieces_partie.indexOf(conexionToDraw.Piece1);
+	        	int startY2 = ConfigPartie.list_pieces_partie.indexOf(conexionToDraw.Piece2);
+	        	if(startY1 > startY2)
+	        	{
+	        		int temp = startY1;
+	        		startY1 = startY2;
+	        		startY2 = temp;
+	        	}
+	        	int gap = startY2 - startY1;
+	
+	        	g2.drawArc((120 - (gap * 10)) - (10 * gap),  150 + startY1 * 100, (120 - (gap * 10)) + (10 * gap), gap * 100, 90, 180);
+	        }
         }
 
         //la première ligne est donc les moments
@@ -195,16 +252,22 @@ public class Problem {
                 if(num_moment != ConfigPartie.list_moments_partie.size())
                 	DrawTools.drawImageCenter(graph_tableau,img_carre,x_lieu,y_lieu);
                 else
-                	DrawTools.drawImageTransformed(graph_tableau, img_carre_bord, x_lieu, y_lieu, 270, 100);
+                {
+                	if(displayBorder)
+                		DrawTools.drawImageTransformed(graph_tableau, img_carre_bord, x_lieu, y_lieu, 270, 100);
+                }
             }
             y_lieu = y_lieu+100 ;//l increment de y_lieu pour la ligne suivante.
         }
         //derniere ligne
-        int x_ligne=250;
-        int y_ligne=((ConfigPartie.list_pieces_partie.size()+1)*100)+50;
-        for (int num_moment=0 ; num_moment<ConfigPartie.list_moments_partie.size() ; num_moment++){
-        	DrawTools.drawImageTransformed(graph_tableau, img_carre_bord, x_ligne, y_ligne, 0, 100);
-            x_ligne=x_ligne+100;
+        if(displayBorder)
+        {
+	        int x_ligne=250;
+	        int y_ligne=((ConfigPartie.list_pieces_partie.size()+1)*100)+50;
+	        for (int num_moment=0 ; num_moment<ConfigPartie.list_moments_partie.size() ; num_moment++){
+	        	DrawTools.drawImageTransformed(graph_tableau, img_carre_bord, x_ligne, y_ligne, 0, 100);
+	            x_ligne=x_ligne+100;
+	        }
         }
 
         return image_tableau;
--- src/SmartLogic.java
+++ src/SmartLogic.java
@@ -6,8 +6,10 @@ public final class SmartLogic {
     public static void main(String[] args) {
         String nom_export = ConfigPartie.nom_export;
         Problem p = new Problem();
+        p.exportVierge(nom_export + "_EMPTY");
         p.export(nom_export, false);
-        p.export("Expert "+nom_export, true);
+        p.export(nom_export + "_Expert", true);
+        p.exportSolution(nom_export + "_SOL");
         System.out.println(p);
         System.out.println("Nom export : "+nom_export);
     }
